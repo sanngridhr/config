dbbceb546257e8a220e03d7d5bd24fc9d2830323
Orest58008 <orest58008@tuta.io>
2023-03-03 21:23:54 +0200

    config.fish changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
-    alias fish_greeting /data/projects/sysfetch/sysfetch
+   # aliases
+    alias fish_greeting yass
+    alias cat=bat
+    alias ls="exa -lFh --git"
+
+   # abbreviations
     abbr a "abbr"
     abbr i "yay -Si"
     abbr R "yay -Rns"
@@ -10,5 +15,11 @@ if status is-interactive
     abbr q "exit"
     abbr s "sudo"
 
+   # variaples
+    set -gx PAGER most
+    set -gx EDITOR nvim
+    fish_add_path ~/.local/bin/
+
+   # starship
     starship init fish | source
 end
