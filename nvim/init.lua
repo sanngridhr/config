09b4e9290170ae8d435540e09640b51c3a366134
Orest I <orest58008@yamadachi.quest>
2024-05-03 15:58:11 +0300

    Update nvim
    Signed-off-by: Orest I <orest58008@yamadachi.quest>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,62 +1,100 @@
--- vim: sw=2
+--- nvim setup
+-- line numbers
+vim.opt.number = true
+-- proper line breaks
+vim.opt.linebreak = true
+-- tab width
+vim.opt.sw = 3
+vim.opt.ts = 3
+-- clipboard
+vim.opt.clipboard = 'unnamedplus'
 
--- nvim setup
-vim.cmd [[set number]]
-vim.cmd [[set sw=4]]
-
--- lazy.nvim setup
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+--- lazy.nvim setup
+local lazypath = vim.fn.stdpath'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+	vim.fn.system {
+		'git', 'clone', '--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
 end
 vim.opt.rtp:prepend(lazypath)
 
-require'lazy'.setup({
-  {
-    'nvim-orgmode/orgmode',
-    event = 'VeryLazy',
-    ft = { 'org' },
-    config = function()
-      require'orgmode'.setup({
-	org_agenda_files = '~/orgfiles/**/*',
-	org_default_notes_file = '~/orgfiles/refile.org',
-      })
-    end,
-  },
-  { 'jiangmiao/auto-pairs', name = 'auto-pairs' },
-  {
-    'kepano/flexoki-neovim',
-    name = 'flexoki',
-    config = function()
-      vim.cmd('colorscheme flexoki-dark')
-    end,
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require'lualine'.setup()
-    end,
-  },
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      local lc = require'lspconfig'
-      lc.denols.setup{}
-    end,
-  },
-  {
-    'ervandew/supertab',
-    name = 'supertab',
-    config = function()
-      vim.cmd [[let g:SuperTabDefaultCompletionType = "<c-n>"]]
-    end,
-  }
-})
+require'lazy'.setup {
+	{
+		'jiangmiao/auto-pairs'
+	},
+	{
+		'kepano/flexoki-neovim',
+		config = function()
+			vim.cmd('colorscheme flexoki-dark')
+		end,
+	},
+	{
+		'nvimdev/dashboard-nvim',
+		event = 'VimEnter',
+		config = function()
+			require'dashboard'.setup {
+				shortcut_type = 'number',
+			}
+		end,
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require'lualine'.setup()
+		end,
+	},
+	{
+		'echasnovski/mini.completion',
+		version = '*',
+		config = function()
+			vim.keymap.set('i', '<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { expr = true })
+			vim.keymap.set('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
+			require'mini.completion'.setup {}
+		end
+	},
+	{
+		'neovim/nvim-lspconfig',
+		config = function()
+			local lc = require'lspconfig'
+			-- list of supported servers:
+			--   https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+			lc.denols.setup {}
+		end,
+	},
+	{
+		'nvim-orgmode/orgmode',
+		event = 'VeryLazy',
+		ft = { 'org' },
+		config = function()
+			require'orgmode'.setup({
+				org_startup_folded = 'content',
+				org_hide_leading_stars = true,
+			})
+		end,
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			local builtin = require'telescope.builtin'
+			vim.keymap.set('n', '<C-o>', vim.cmd('cd /data') and builtin.find_files)
+		end
+	},
+}
+
+--- neovide setup
+if vim.g.neovide then
+	-- fix font height
+	vim.opt.guifont = 'monospace,CaskaydiaCove Nerd Font:h11'
+	-- remove awful default animations
+	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_scroll_animation_length = 0
+	-- require quit confirmation on unsaved
+	vim.g.neovide_confirm_quit = true
+end
