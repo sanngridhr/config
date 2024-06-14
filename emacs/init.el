e7a9941f6a9d2753ba2ff2451411cce2208a0aa8
Orest I <orest58008@yamadachi.quest>
2024-06-14 23:31:12 +0300

    Update emacs, sway
    Signed-off-by: Orest I <orest58008@yamadachi.quest>

--- emacs/init.el
+++ emacs/init.el
@@ -1,29 +1,44 @@
-; NATIVE SETTINGS
+					; NATIVE SETTINGS
+;; default tab width
+(setq-default tab-width 4)
+
+;; disable annoying bars
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; fill column
+(setopt fill-column 99)
+(global-display-fill-column-indicator-mode)
+
 ;; font
 (set-frame-font "monospace 13" nil t)
+
 ;; hooks
 (add-hook 'prog-mode-hook 'electric-pair-mode)
-; (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+;; minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook (lambda ()
+				(setq gc-cons-threshold (expt 2 32))))
+
 ;; numbers
 (global-display-line-numbers-mode)
 (column-number-mode)
-;; tool-bar
-(tool-bar-mode -1)
-
-; PACKAGE SETUP
+
+					; PACKAGE SETUP
 ;; straight.el
 (defvar bootstrap-version)
 (let ((bootstrap-file
-        (expand-file-name
-          "straight/repos/straight.el/bootstrap.el"
-          (or (bound-and-true-p straight-base-dir)
-              user-emacs-directory)))
+       (expand-file-name
+	"straight/repos/straight.el/bootstrap.el"
+	(or (bound-and-true-p straight-base-dir)
+	    user-emacs-directory)))
       (bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-      (url-retrieve-synchronously
-        "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-        'silent 'inhibit-cookies)
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -31,8 +46,8 @@
 ;;; enable straight-use-package
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
-
-; PACKAGES
+
+					; PACKAGES
 ;; company
 (use-package company
   :hook (prog-mode . company-mode))
@@ -44,13 +59,23 @@
   (dashboard-startup-banner 'logo)
   (dashboard-show-shortcuts t))
 
+;; deno-fmt
+(use-package deno-fmt
+  :hook (js2-mode . deno-fmt-mode)
+  :hook (typescript-ts-mode . deno-fmt-mode))
+
+;; eglot
+(use-package eglot
+  :hook (deno-fmt-mode . eglot-ensure))
+
 ;; elcord
 (use-package elcord
   :config (elcord-mode))
 
 ;; evil
 (use-package evil
-  :config (evil-mode 1))
+  :config (evil-mode 1)
+  :custom (evil-undo-system 'undo-redo))
 
 ;; flexoki-themes
 (use-package flexoki-themes
@@ -66,7 +91,7 @@
   :config
   (ligature-set-ligatures 't '("ff" "fi" "ffi"))
   (ligature-set-ligatures 't '("|||>" "<|||" "<==>" "<!--" "<!---->" "####" "~~>" "***" "||="
-			       "||>" ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+			       "||>" ":::" "::=" "=:=" "===" "==>" "=>>" "=<<" "=/=" "!==" ; "=!="
 			       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<" "<~~"
 			       "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->" "<--" "<-<"
 			       "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<" "..." "+++" "/=="
@@ -78,9 +103,12 @@
 			       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:" "?="
 			       "?." "??" ";;" "/*" "/**" "/=" "/>" "//" "__" "~~" "(*" "*)"
 			       "\\\\" "://" "}}" "{{" ">=" "<=" "[TODO)" "todo))" ))
-  :hook (prog-mode . ligature-mode)
-  :hook (conf-mode . ligature-mode))
+  :hook (conf-mode . ligature-mode)
+  :hook (prog-mode . ligature-mode))
 
-;; page-break-lines
-(use-package page-break-lines
-  :config (global-page-break-lines-mode))
+;; projectile
+(use-package projectile
+  :init (projectile-mode +1))
+
+;; rainbow-mode
+(use-package rainbow-mode)
