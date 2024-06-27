64bb4e2c5f6b13a47e6ba3ed0daa244e0a55d387
Orest I <orest58008@yamadachi.quest>
2024-06-27 16:56:22 +0300

    Update emacs, zsh
    Signed-off-by: Orest I <orest58008@yamadachi.quest>

--- emacs/init.el
+++ emacs/init.el
@@ -1,13 +1,14 @@
-					; NATIVE SETTINGS
-;; default tab width
+                                        ; NATIVE SETTINGS
+;; tabs
 (setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
 
 ;; disable annoying bars
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 ;; fill column
-(setopt fill-column 99)
+(setopt fill-column 95)
 (global-display-fill-column-indicator-mode)
 
 ;; font
@@ -15,30 +16,32 @@
 
 ;; hooks
 (add-hook 'prog-mode-hook 'electric-pair-mode)
+(add-hook 'org-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 ;; minimize garbage collection during startup
 (setq gc-cons-threshold most-positive-fixnum)
 (add-hook 'emacs-startup-hook (lambda ()
-				(setq gc-cons-threshold (expt 2 32))))
+				                (setq gc-cons-threshold (expt 2 32))))
 
 ;; numbers
 (global-display-line-numbers-mode)
 (column-number-mode)
 
-					; PACKAGE SETUP
+                                        ; PACKAGE SETUP
 ;; straight.el
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
-	"straight/repos/straight.el/bootstrap.el"
-	(or (bound-and-true-p straight-base-dir)
-	    user-emacs-directory)))
+	    "straight/repos/straight.el/bootstrap.el"
+	    (or (bound-and-true-p straight-base-dir)
+	        user-emacs-directory)))
       (bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
+	    (url-retrieve-synchronously
+	     "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	     'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -47,7 +50,7 @@
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
 
-					; PACKAGES
+                                        ; PACKAGES
 ;; company
 (use-package company
   :hook (prog-mode . company-mode))
@@ -58,10 +61,6 @@
   :custom
   (dashboard-startup-banner 'logo)
   (dashboard-show-shortcuts t))
-
-;; eglot
-(use-package eglot
-  :hook (deno-fmt-mode . eglot-ensure))
 
 ;; elcord
 (use-package elcord
@@ -78,27 +77,29 @@
 
 ;; hl-todo
 (use-package hl-todo
-  :hook (org-mode . hl-todo-mode)
-  :hook (prog-mode . hl-todo-mode))
+  :hook ((org-mode prog-mode)
+         . hl-todo-mode))
 
 ;; ligature
 (use-package ligature
   :config
   (ligature-set-ligatures 't '("ff" "fi" "ffi"))
   (ligature-set-ligatures 't '("|||>" "<|||" "<==>" "<!--" "<!---->" "####" "~~>" "***" "||="
-			       "||>" ":::" "::=" "=:=" "===" "==>" "=>>" "=<<" "=/=" "!==" ; "=!="
-			       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<" "<~~"
-			       "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->" "<--" "<-<"
-			       "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<" "..." "+++" "/=="
-			       "///" "_|_" "www" "&&" "^=" "~~" "~@" "~=" "~>" "~-" "**" "*>"
-			       "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|" "[|" "]#" "::" ":*" ":+"
-			       ":^" ":-" ":~" ":=" ":>" "::>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
-			       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:" "<$"
-			       "<=" "<>" "<-" "<<" "<+" "</" "#{" "}#" "#[" "]#" "#:" "#=" "#!"
-			       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:" "?="
-			       "?." "??" ";;" "/*" "/**" "/=" "/>" "//" "__" "~~" "(*" "*)"
-			       "\\\\" "://" "}}" "{{" ">=" "<=" "[TODO)" "todo))" ))
+                               "||>" ":::" "::=" "=:=" "===" "==>" "=>>" "=<<" "=/=" "!=="
+                               "=!=" "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---"
+                               "-<<" "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<"
+                               "<->" "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_("
+                               "..<" "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@"
+                               "~=" "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                               "[|" "]#" "::" ":*" ":+" ":^" ":-" ":~" ":=" ":>" "::>" ":<"
+                               "$>" "==" "=>" "!=" "!!" ">:" ">=" ">>" ">-" "-~" "-|" "->" "--"
+                               "-<" "<~" "<*" "<|" "<:" "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{"
+                               "}#" "#[" "]#" "#:" "#=" "#!" "##" "#(" "#?" "#_" "%%" ".=" ".-"
+                               ".." ".?" "+>" "++" "?:" "?=" "?." "??" ";;" "/*" "/**" "/="
+                               "/>" "//" "__" "~~" "(*" "*)" "\\\\" "://" "}}" "{{" ">=" "<="
+                               "[TODO)" "todo))" ))
   :hook (conf-mode . ligature-mode)
+  :hook (org-mode . ligature-mode)
   :hook (prog-mode . ligature-mode))
 
 ;; projectile
@@ -111,7 +112,11 @@
 										; LANGUAGES
 ;; LSP
 (use-package lsp-mode
-  :hook (typescript-mode))
+  :hook ((go-mode typescript-mode)
+         . lsp-mode))
+
+;; Go
+(use-package go-mode)
 
 ;; TypeScript
 (use-package typescript-mode)
