7788e7b7d8998ceb55c2311bbed908f0a6f3b0fd
Orest Ivanko <orest58008@yamadachi.quest>
2025-07-11 17:52:57 +0300

    remove unneeded
    Signed-off-by: Orest Ivanko <orest58008@yamadachi.quest>

--- flake.nix
+++
@@ -1,7 +0,0 @@
-{
-  description = "Master's precious dotfiles! ✧(≖ ◡ ≖✿)";
-
-  outputs = { self }: {
-    # Empty outputs is fine for a dotfiles repo!
-  };
-}
--- waybar/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PATH=$PATH:$HOME/.local/bin
-
-if command -v mycofetch &> /dev/null; then
-	echo $(mycofetch -i "#distro:logo_tiny# ") | sed "s/\x1b\[0m//"
-else
-	echo " "
-fi
